87b59087e69b9d73177e79c031b108e881ef4fd3
Rstinkho <t.turchinskiy@bk.ru>
2018-12-16 19:36:39 +0800

    css tweaks

--- src/BusinessCanvas/BusinessCanvas.js
+++ src/BusinessCanvas/BusinessCanvas.js
@@ -45,24 +45,25 @@ const BusinessCanvas = (props) => {
   <div className="container-fluid">
     <div id="canvas" className="row row-md-12">
       <div id="canvas_screen" className="col col-md-9">
-          <div id="verticalHeader" className="row row-md-12 mb-4"> 
-            <div className="col-md-5">
+          <div id="verticalHeader" className="row row-md-12 mb-4">
+            <div className="col-md-8">
               <h1>The Business Model Canvas</h1>
-            </div>  
+            </div>
 
-            <div className="col-md-7">     
-              <div className="row">  
-                  <div className="col-sm-6 col-md-3"><label>Designer For:</label><input/> </div>
-                  <div className="col-sm-6 col-md-3"><label>By:</label><input /></div>
-                  <div className="col-sm-6 col-md-3"><label>Date:</label><input /></div>
-                </div>  
+            <div className="col-md-4 mt-auto">
+              <div className="row">
+                  <div className="col-sm-6 col-md-3 m-2 mr-3"><input placeholder="Designer For:"/> </div>
+                  <div className="col-sm-6 col-md-3 m-2 mr-3"><input placeholder="By:" /></div>
+                  <div className="col-sm-6 col-md-3 m-2"><input placeholder="Date:" /></div>
+                </div>
             </div>
 
           </div>
-              
+
           <div id="verticalFirst" className="row row-md-12 mr-5 ml-5 mb-3">
               <div id="KP" className="col-sm-2 col-md-2">
                   <h3> <span id="keyPartners" onClick={props.click}>Key Partners</span> </h3>
+                  <hr/>
                   <ul>
                     <li>
                       Operational partners: {partners[0]}
@@ -77,17 +78,20 @@ const BusinessCanvas = (props) => {
                 <div id="activRes" className="row mr-2 ml-2">
                   <div id="KA" className="col-12 mb-2" onClick={props.click}>
                   <h3> <span id ="keyActivities">Key Activities</span> </h3>
+                  <hr/>
                     {activities}
-                  </div> 
-                  <div id="KR" className="col-12" onClick={props.click}> 
+                  </div>
+                  <div id="KR" className="col-12" onClick={props.click}>
                     <h3> <span id="keyResources">Key Resources</span> </h3>
+                    <hr/>
                     {resources}
-                  </div> 
+                  </div>
                 </div>
               </div>
 
               <div id="VP" className="col-sm-2" onClick={props.click}>
                 <h3> <span id="valueProposition">Value Proposition</span> </h3>
+                <hr/>
                 {values}
               </div>
 
@@ -95,6 +99,7 @@ const BusinessCanvas = (props) => {
                 <div id="activRes" className="row mr-2 ml-2">
                   <div id="CH" className="col-12 mb-2" onClick={props.click}>
                     <h3> <span id="channels">Channels</span> </h3>
+                    <hr/>
                     <ul>
                     <li>
                       Distribution channels: {channels[0]}
@@ -103,9 +108,11 @@ const BusinessCanvas = (props) => {
                       Marketing channels: {channels[1]}
                     </li>
                   </ul>
-                  </div> 
-                  <div id ="CR" className="col-12" onClick={props.click}> 
+                  </div>
+                  <div id ="CR" className="col-12" onClick={props.click}>
                     <h3> <span id ="customerRel">Customer Relationships</span> </h3>
+                    <hr/>
+                    <p>(need to fix space between information)</p>
                     <p><font color="blue">{cusRels[0]}</font></p>
                     {cusRels[1]}
                     {cusRels[2]}
@@ -119,23 +126,26 @@ const BusinessCanvas = (props) => {
                     {cusRels[10]}
                     {cusRels[11]}
                     {cusRels[12]}
-                  </div> 
+                  </div>
                 </div>
               </div>
 
             <div id="CS" className="col col-sm-2" onClick={props.click}>
               <h3> <span id="customerSeg">Customer Segments</span> </h3>
+              <hr/>
               {cusSegs}
               </div>
           </div>
-          
+
           <div id="verticalSecond" className="row row-md-12 ml-5 mr-5">
             <div id="CoS" className="col col mr-2" onClick={props.click}>
               <h3> <span id="costStructure">Cost structure</span> </h3>
+              <hr/>
               {costs}
-            </div> 
+            </div>
             <div id="RS" className="col col" onClick={props.click}>
               <h3> <span id="revenueStream">Revenue stream</span> </h3>
+              <hr/>
               {revenues}
             </div>
           </div>
@@ -146,93 +156,93 @@ const BusinessCanvas = (props) => {
       func = {props.fill}
       form = {props} />
       </div>
-    </div>  
-    <div id="verticalBottom" className="row row-md-12 mt-4 fixed-bottom justify-content-center"> 
-            
+    </div>
+    <div id="verticalBottom" className="row row-md-12 mt-4 fixed-bottom justify-content-center">
+
             <div className="col col-auto text-center mr-4">
-              
+
               <label>
-                
+
                 <input type="checkbox" name="" />
                 <span className="check"></span>
                 <p>Step 1</p>
               </label>
             </div>
             <div className="col col-auto text-center mr-4">
-              
+
               <label>
-                
+
                 <input type="checkbox" name="" />
                 <span className="check"></span>
                 <p>Step 2</p>
               </label>
-            </div> 
+            </div>
             <div className="col col-auto text-center mr-4">
-              
+
               <label>
-                
+
                 <input type="checkbox" name="" />
                 <span className="check"></span>
                 <p>Step 3</p>
               </label>
-            </div> 
+            </div>
             <div className="col col-auto text-center mr-4">
-              
+
               <label>
-                
+
                 <input type="checkbox" name="" />
                 <span className="check"></span>
                 <p>Step 4</p>
               </label>
             </div>
             <div className="col col-auto text-center mr-4">
-              
+
               <label>
-                
+
                 <input type="checkbox" name="" />
                 <span className="check"></span>
                 <p>Step 5</p>
-                
+
               </label>
-            </div> 
+            </div>
             <div className="col col-auto text-center mr-4">
-              
+
               <label>
-                
+
                 <input type="checkbox" name="" />
                 <span className="check"></span>
                 <p>Step 6</p>
               </label>
-            </div>   
+            </div>
             <div className="col col-auto text-center mr-4">
-              
+
               <label>
-                
+
                 <input type="checkbox" name="" />
                 <span className="check"></span>
                 <p>Step 7</p>
               </label>
             </div>
             <div className="col col-auto text-center mr-4">
-              
-              <label>             
+
+              <label>
                 <input type="checkbox" name="" />
-                
+
                 <span className="check"></span>
                 <p>Step 8</p>
               </label>
-            </div> 
+            </div>
             <div className="col col-auto text-center mr-4">
-              
-              <label className="col align-items-center">             
+
+              <label className="col align-items-center">
                 <input type="checkbox" name="" />
-                
+
                 <span className="check"></span>
                 Step 9
               </label>
-            </div>       
+            </div>
 
-          </div> 
+          </div>
   </div>
   )
 }
