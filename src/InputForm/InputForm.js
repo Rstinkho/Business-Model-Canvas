3f9ab55d985118e309efc60ee49977354a1bf6f3
Rstinkho <rstinkho@bk.ru>
2018-12-18 18:50:44 +0800

    minors

--- src/InputForm/InputForm.js
+++ src/InputForm/InputForm.js
@@ -21,6 +21,17 @@ const form = (props) => {
         var twoSidedStatic = $("#twoSided");
         var twoSidedStatic_1 = $("#twoSided_1");
 
+        // 3. cost structure
+        var wrapper_cs	= $("#hcapital");
+        var wrapper2_cs = $("#inventory");
+        var wrapper3_cs = $("#technology");
+        var wrapper4_cs = $("#other");
+        var staticOne_cs = $("#static_1cs");
+        var staticTwo_cs = $("#static_2cs");
+        var staticThree_cs = $("#static_3cs");
+        var staticFour_cs = $("#static_4cs");
+
+        
         // jQuery functions for removing unneccessary elements of canvas
 
         // 1. customer's relationships
@@ -51,6 +62,23 @@ const form = (props) => {
             $(twoSidedStatic_1).val(null);
         })
 
+        // 3. cost structure
+        $(wrapper_cs).on("click",".removeFieldcs", function(e){
+            e.preventDefault(); $(wrapper_cs).hide();
+            $(staticOne_cs).val(null);
+        })
+        $(wrapper2_cs).on("click",".removeField_2cs", function(e){
+            e.preventDefault(); $(wrapper2_cs).hide();
+            $(staticTwo_cs).val(null);
+          })
+        $(wrapper3_cs).on("click",".removeField_3cs", function(e){
+            e.preventDefault(); $(wrapper3_cs).hide();
+            $(staticThree_cs).val(null);
+          })
+        $(wrapper4_cs).on("click",".removeField_4cs", function(e){
+            e.preventDefault(); $(wrapper4_cs).hide();
+            $(staticFour_cs).val(null);
+        })
       });
 
     if (props.form.info.form.currentId === "keyActivities") {
@@ -233,24 +261,24 @@ const form = (props) => {
             <h4>#{props.form.info.form.currentId}</h4>
             <p>What is your primary business cost?</p>
 
-            <div className="input-box" id ="transaction">
-                    <input type="text" id="static_1" value="Human capital:" disabled></input>
-                    <input id="P1" type="text" name="mytext" placeholder="$" /> <button className="removeField">X</button>
+            <div className="input-box" id ="hcapital">
+                    <input type="text" id="static_1cs" value="Human capital:" disabled></input>
+                    <input id="P1" type="text" name="mytext" placeholder="$" /> <button className="removeFieldcs">X</button>                
             </div>
 
-            <div className="input-box" id="longterm">
-                    <input type="text" id="static_2" value="Inventory:" disabled></input>
-                    <input id="P1" type="text" name="mytext" placeholder="$" /> <button className="removeField_2">X</button>
+            <div className="input-box" id="inventory">
+                    <input type="text" id="static_2cs" value="Inventory:" disabled></input>
+                    <input id="P1" type="text" name="mytext" placeholder="$" /> <button className="removeField_2cs">X</button>              
             </div>
 
-            <div className="input-box" id="community">
-                    <input type="text" id="static_3" value="Technology:" disabled></input>
-                    <input id="P1" type="text" name="mytext" placeholder="$" /> <button className="removeField_3">X</button>
+            <div className="input-box" id="technology">
+                    <input type="text" id="static_3cs" value="Technology:" disabled></input>
+                    <input id="P1" type="text" name="mytext" placeholder="$" /> <button className="removeField_3cs">X</button>                   
             </div>
 
-            <div className="input-box" id="personal">
-                    <input type="text" id="static_4" value="Other:" disabled></input>
-                    <input id="P1" type="text" name="mytext" placeholder="add description" /> <button className="removeField_4">X</button>
+            <div className="input-box" id="other">
+                    <input type="text" id="static_4cs" value="Other:" disabled></input>
+                    <input id="P1" type="text" name="mytext" placeholder="add description" /> <button className="removeField_4cs">X</button>                 
             </div>
             <div className="input-box">
             <p>What is your secondary business cost?</p>
@@ -262,8 +290,8 @@ const form = (props) => {
         )
     }  else {
         return (
-            <form name="chatform"onSubmit={props.func} >
-            <h4>{props.form.info.form.title}</h4>
+            <form name="chatform" onSubmit={props.func} >
+            <h4>{props.form.info.form.title}</h4>  
             </form>
         )
     }
